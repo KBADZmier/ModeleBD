9808f7c50fe57af8c1bbac5d5f1c9915d4faf773
KBADZmier <109542805+KBADZmier@users.noreply.github.com>
2023-04-07 19:00:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using ModeleBD;
+using System.Data.SqlClient;
+namespace ModeleBD;
+public class PozycjeZamowienia
+{
+    public int IDZamowienia { get; set; }
+    public int IDProduktu { get; set; }
+    public int Ilosc { get; set; }
+}
+
+public class Program 
+{
+   
+    public static void Main(string[] arg )
+    {
+string connectionString = @"Data Source=DESKTOP-IJ0SFAF\SQLEXPRESS;Initial Catalog=Modele;Integrated Security=True";
+string insertSql = "select* from PozycjeZamówienia";
+
+using (var connection = new SqlConnection(connectionString))
+{
+    var pozycjeZamowienia = connection.Query<PozycjeZamowienia>(insertSql);
+    foreach(var item in pozycjeZamowienia)
+     {
+
+       Console.WriteLine($"{item.IDZamowienia} {item.IDProduktu} {item.Ilosc}");
+
+      }
+
+
+
+
+}
+
+
+    }
+
+
+}
+
